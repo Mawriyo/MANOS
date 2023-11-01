579db1ef0c7e0cbaadc581e7393813dd21767d84
ken <kmelgar@psasystems.com>
2023-11-01 15:29:27 -0400

    WednesdayDEmo11_1

--- src/castelet/castelet/castelet.py
+++ src/castelet/castelet/castelet.py
@@ -8,8 +8,10 @@ from curses import COLOR_RED
 from functools import partial
 from math import pow, atan2, sqrt
 import math
+import threading
+import time
 import cv2
-from castelet.turtleSimControl.TurtleSimControl import TurtleSimControl
+from src.castelet.turtleSimControl.TurtleSimControl import TurtleSimControl
 import rclpy
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 import sys
@@ -27,75 +29,89 @@ from turtlesim.srv import TeleportAbsolute, TeleportRelative, Spawn
 from std_msgs.msg import Int16
 
 
-class Castlet(QMainWindow):
+class Castlet(QMainWindow,):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
         self.node = rclpy.create_node('castelet_node')
+        # self.turtleSimControl = rclpy.
         self.menuSelection = ""
         self.handTxt = ""
         self.fingerTxt=""      
         self.handbinding=""
+        self.services = []
         
         self.castelet_ui = Ui_CaseletWindow()
         self.castelet_ui.setupUi(self)
+        self.castelet_ui.test.clicked.connect(self.getServiceList)
         self.qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT,
                                  durability=DurabilityPolicy.VOLATILE,
                                  history=HistoryPolicy.KEEP_LAST,
                                  depth=1)
-        
+        self.castelet_ui.menuLeft_Hand.aboutToShow.connect(partial(self.handSelection, '/MANOS/Left_Hand/'))
+        self.castelet_ui.menuRight_Hand.aboutToShow.connect(partial(self.handSelection, '/MANOS/Right_Hand/'))
         self.subscriptions = {
           #  'left_hand': self.node.create_subscription(Pos, '/MANOS/Left_Hand/Pointer_pos', self.test, self.qos_profile),
            # 'rhand': self.node.create_subscription(Pos, '/MANOS/Right_Hand/Pointer_pos', self.test, self.qos_profile),
             'Cam':  self.node.create_subscription(Image, '/MANOS/camera/raw_image', self.image_callback , self.qos_profile),
-            'Skel':  self.node.create_subscription(Image, '/MANOS/camera/hand_pos', self.test, self.qos_profile),
+            #'Skel':  self.node.create_subscription(Image, '/MANOS/camera/hand_pos', self.test, self.qos_profile),
+            'temp':  self.node.create_subscription(ListString, '/MANOS/ServiceDetector', self.test, self.qos_profile),
+
         }
         self.spawned_turtles = []  
         self.namespace = 'turtle1' #Roslaunch script param? 
 
 
         self.velocity_publisher = self.node.create_publisher(Twist, '/'+ self.namespace + '/cmd_vel', 2)
-        # self.raw_image_subscription = self.node.create_subscription(Image, '/MANOS/camera/raw_image', self.image_callback, self.qos_profile)
-
-        self.pose_subscriber = self.node.create_subscription(TPose, '/'+ self.namespace + '/pose', self.update_pose, self.qos_profile)
-        self.binding_option_subscriber = self.node.create_subscription(ListString, '/MANOS/ServiceDetector', self.getBindings,self.qos_profile)
-        self.teleport_service = self.node.create_client(TeleportAbsolute, '/'+ self.namespace + '/teleport_absolute')
+        # self.pose_subscriber = self.node.create_subscription(TPose, '/'+ self.namespace + '/pose', self.update_pose, self.qos_profile)
+        self.binding_option_subscriber = self.node.create_subscription(ListString, '/MANOS/ServiceDetector', self.getBindings, self.qos_profile)
+        # self.teleport_service = self.node.create_client(TeleportAbsolute, '/'+ self.namespace + '/teleport_absolute')
         self.spawnTurtle = self.node.create_client(Spawn, 'spawn' )
+        self.turtleSimControl = TurtleSimControl()
+
         self.topics = []
         self.bridge = CvBridge()
         self.ros_timer = QTimer(self)
         self.ros_timer.timeout.connect(self.process_ros_messages)
         self.ros_timer.start(30)
+
+        # t1 = threading.Thread(target=self.process_ros_messages2)
+        # t1.start()
         self.temp = False
-        self.teleportNode = self.node.create_subscription(TPose, '/'+ self.namespace + '/pose', self.update_pose, 2)
-      #  self.rotateTurtle_sub = self.node.create_subscription(Pos, '/MANOS/Right_Hand/Pointer_pos', self.rotateTurtle, self.qos_profile)
-       # self.telePortTurtle_sub = self.node.create_subscription(Pos, '/MANOS/Left_Hand/Pointer_pos', self.teleportMethod, self.qos_profile)
+        
 
 ###################################################################################################
-#
-#                                      MENU OPTIONS
-#   
+#                                                                                                 #
+#                                      MENU OPTIONS                                               #
+#                                                                                                 #
 ###################################################################################################
-
         self.castelet_ui.menuBindings.aboutToShow.connect(self.refreshBindings)
         self.castelet_ui.actionTurtleSim.triggered.connect(self.TurtleDemo)
         #    def change_callback(self, sub_key, topic_type, topic, function):
 
     def test(self,msg):
+        # print(msg) 
+        # print(str(msg.data))
         pass
+   #     self.filtered_service_list = [(service_name, service_type) for service_name, service_type in  self.services
+                                # if any(keyword in service_name for keyword in self.keywords)]
+
+    def getServiceList(self):
+        return self.services
 
     def refreshBindings(self):
         menu_bindings = self.castelet_ui.menuBindings
         self.handTxt = ""
         self.fingerTxt = ""
-        
-        self.castelet_ui.menuLeft_Hand.aboutToShow.connect(partial(self.handSelection, '/MANOS/Left_Hand/'))
-        self.castelet_ui.menuRight_Hand.aboutToShow.connect(partial(self.handSelection, '/MANOS/Right_Hand/'))
-        child_widgets = menu_bindings.findChildren(QMenu)  # Children of binding Lhand and RHand
+        child_widgets = menu_bindings.findChildren(QMenu) 
+         
         for widget in child_widgets:
-                    widget.triggered.connect(partial(self.handSelection, widget.title()))
-                    sub_children = widget.findChildren(QMenu)
-                    for sub_widget in sub_children:
-                        sub_widget.aboutToShow.connect(partial(self.fingerSelection, sub_widget.title()+"_pos",sub_widget))
+        # Check if connections already exist and only establish new connections if they don't
+            if not widget.signalsBlocked():
+                widget.triggered.connect(partial(self.handSelection, widget.title()))
+                sub_children = widget.findChildren(QMenu)
+
+                for sub_widget in sub_children:
+                  sub_widget.aboutToShow.connect(partial(self.fingerSelection, sub_widget.title()+"_pos",sub_widget))
 
     def handSelection(self, text): 
         self.handTxt=text   
@@ -104,17 +120,28 @@ class Castlet(QMainWindow):
         self.fingerTxt=text      
         self.handbinding = self.handTxt + self.fingerTxt
         sub_widget.clear()
-        for topic in self.topics:
+
+        for topic in self.turtleSimControl.servicess:
+            print(str(topic))
             if "/MANOS/" not in topic and "/rosout" not in topic:
                 action = QAction(sub_widget)
                 action.setText(topic)
                 #/MANOS/left_hand/pointer_pos
-                action.triggered.connect(partial(self.change_callback, self.handbinding, Pos, self.handbinding, self.teleportMethod))
+                action.triggered.connect(partial(self.change_callback, self.handbinding, Pos, self.handbinding, self.turtleSelection))
                 sub_widget.addAction(action)
 
 
     def getBindings(self,msg):
-        self.topics = msg.data
+        # print("FROM DIRECT TOPIC" + str(msg.data))
+        # print("FROMT TURTLECONTROLNODE INSTANCE" + str(self.turtleSimControl.getServices()))
+        self.turtleSimControl.initialize()
+        if not self.temp:
+            rclpy.spin(self.turtleSimControl)
+            self.temp = True
+        print("FROMT BEFORE INSTANCE" )
+        print(str(self.turtleSimControl.getServiceList()))
+        print("FROMT TURTLECONTROLNODE INSTANCE" )
+
      
 
     def change_callback(self, sub_key, topic_type, topic, function):
@@ -139,144 +166,36 @@ class Castlet(QMainWindow):
             self.change_callback('Cam', Image, '/MANOS/camera/raw_image', self.image_callback)
             self.node.destroy_subscription(self.subscriptions['Skel'])
 
-        self.turtleSimControl = TurtleSimControl()
-       # self.spawner_subscription = self.node.create_subscription(Int16, '/MANOS/left_hand/fingers_up', self.turtleSelection, 2)
+        # Pass the Node instance to TurtleSimControl
+ 
 
+    # def turtleSelection(self, msg):
+    #     val = msg.data
+    #     if val == 1:
+    #         self.namespace = 'turtle1'
 
+    #     elif val == 2:
+    #         self.namespace = 'turtle2'
+    #         if 'turtle2' not in self.spawned_turtles:
+    #             self.spawned_turtles.append('turtle2')
+    #             self.spawn_turtle('turtle2', 5.0, 3.0, 0.0)
+    #     elif val == 3:
+    #         self.namespace = 'turtle3'
+    #         if 'turtle3' not in self.spawned_turtles:
+    #            self.spawned_turtles.append('turtle3')
+    #            self.spawn_turtle('turtle3', 1.0, 4.0, 0.0)
+    #     elif val == 4 :
+    #         self.namespace = 'turtle4'
+    #         if 'turtle4' not in self.spawned_turtles:
+    #             self.spawned_turtles.append('turtle4')
+    #             self.spawn_turtle('turtle4', 8.0, 8.0, 0.0)
+    #     elif val == 5:
+    #         self.namespace = 'turtle5'
+    #         if 'turtle5' not in self.spawned_turtles:
+    #            self.spawned_turtles.append('turtle5')
+    #            self.spawn_turtle('turtle5', 8.0, 5.0, 0.0)
+    #     return self.namespace
 
-#################################################################################################################################
-#                           WITH SOME EDITS I CREATED THE FOLLOWING TO ADAPT TO MY IMPLEMENTATION                               #
-#                               http://wiki.ros.org/turtlesim/Tutorials/Go%20to%20Goal
-#  This is going to be Trickier than I thought. Where as the link above demonstrates being able to give it a waypoint, the way
-#  the system publishes is faster than the turtle can move. Hence why the system freezes as the buffer/queue within the system 
-#  is full. You typically see the turtle just go crazy as a result.
-#################################################################################################################################
-
-    def update_pose(self, data):
-        self.pose = TPose()
-        self.pose.x = round(data.x, 4)
-        self.pose.y = round(data.y, 4)
-       
-        
-    def euclidean_distance(self, goal_pose):
-        """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose.position.x - self.pose.x), 2) +
-                    pow((goal_pose.position.y - self.pose.y), 2))
-
-    def linear_vel(self, goal_pose, constant=1.5):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * self.euclidean_distance(goal_pose)
-
-    def steering_angle(self, goal_pose):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return atan2(goal_pose.position.y - self.pose.y, goal_pose.position.x - self.pose.x)
-
-    def angular_vel(self, goal_pose, constant=6):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
-
-    def move2goal(self,msg):
-        """Moves the turtle to the goal."""
-        goal_pose = Pose()
-        goal_pose.position.x = (msg.x/ 640) * 10
-        goal_pose.position.y = (msg.y/480) * 10
-        distance_tolerance =.05
-        vel_msg = Twist()
-
-        while self.euclidean_distance(goal_pose) >= distance_tolerance:
-
-            # Proportional controller.
-            # https://en.wikipedia.org/wiki/Proportional_control
-
-            vel_msg.linear.x = self.linear_vel(goal_pose)
-            vel_msg.angular.z = self.angular_vel(goal_pose)
-            self.velocity_publisher.publish(vel_msg)
-
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-        self.velocity_publisher.publish(vel_msg)
-
-
-###############################################################################
-#                       If I want life to be easy lol.....                        #
-###############################################################################
-# This seems to have changed from ROS1 and ROS2.....
-#In ROS 2, service proxies provide both synchronous and asynchronous ways to 
-# call a service. When you use call_async, you are making an asynchronous
-# service call, which means that your code can continue running while waiting
-# for the service response. On the other hand, if you use call, you are making 
-# a synchronous service call, which blocks until a response is received or a
-# timeout occurs. I 100% can make try catches within here but I am under the 
-# Assumption that the roslaunch script will prevent any sort of issues related 
-# to this service not being available 
-#TODO: I also have to figure out how to get to the bottoom of the world. 
-# For additional context you cannot go to (0,0) nor (0,10) as the hand no longer 
-# can be detected as it becomes out of frames. Possibly adding a calibration feature? 
-
-    def teleportMethod(self, msg):
-        teleport_request = TeleportAbsolute.Request()
-        teleport_request.x = 10- (msg.x / 640) * 10
-        teleport_request.y = 10 - (msg.y / 480) * 10  # Invert the y-coordinate
-        teleport_request.theta = 0.0  # Ensure that the turtle does not change its orientation
-        self.teleport_service.call_async(teleport_request)
-
-    def rotateTurtle(self, msg):
-        request = TeleportAbsolute.Request()
-        request.x = self.pose.x
-        request.y = self.pose.y
-        request.theta = self.calculate_theta(10-(msg.x / 640) * 10, 10 - (msg.y / 480) * 10)  # Invert the y-coordinate
-        self.teleport_service.call_async(request)
-
-
-    def calculate_theta(self, x, y):
-        # Calculate the angle (theta) based on the finger position
-        current_x = self.pose.x # Get the current turtle's x-position here
-        current_y = self.pose.y # Get the current turtle's y-position here
-        delta_x = x - current_x
-        delta_y = y - current_y
-
-        # Calculate the angle using arctangent (in radians)
-        theta = math.atan2(delta_y, delta_x)
-
-        return theta
-
-
-  #Turtle1 will automatically be spawned when \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    def turtleSelection(self, msg):
-        val = msg.data
-        if val == 1:
-            self.namespace = 'turtle1'
-
-        elif val == 2:
-            self.namespace = 'turtle2'
-            if 'turtle2' not in self.spawned_turtles:
-                self.spawned_turtles.append('turtle2')
-                self.spawn_turtle('turtle2', 5.0, 3.0, 0.0)
-        elif val == 3:
-            self.namespace = 'turtle3'
-            if 'turtle3' not in self.spawned_turtles:
-               self.spawned_turtles.append('turtle3')
-               self.spawn_turtle('turtle3', 1.0, 4.0, 0.0)
-        elif val == 4 :
-            self.namespace = 'turtle4'
-            if 'turtle4' not in self.spawned_turtles:
-                self.spawned_turtles.append('turtle4')
-                self.spawn_turtle('turtle4', 8.0, 8.0, 0.0)
-        elif val == 5:
-            self.namespace = 'turtle5'
-            if 'turtle5' not in self.spawned_turtles:
-               self.spawned_turtles.append('turtle5')
-               self.spawn_turtle('turtle5', 8.0, 5.0, 0.0)
-        return self.namespace
-
-    def spawn_turtle(self, name, x, y, theta):
-        request = Spawn.Request()
-        request.name = name
-        request.x = x
-        request.y = y
-        request.theta = theta
-        self.spawnTurtle.call_async(request)
 
         
     def image_callback(self, msg):
@@ -332,6 +251,9 @@ class Castlet(QMainWindow):
     def process_ros_messages(self):
         # Process ROS messages by calling spin_once() in each timer tick
         rclpy.spin_once(self.node)
+
+
+
 
 def main(args=None):
     rclpy.init(args=None)
--- src/castelet/turtleSimControl/TurtleSimControl.py
+++ src/castelet/turtleSimControl/TurtleSimControl.py
@@ -1,47 +1,46 @@
+import math
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from turtlesim.srv import TeleportAbsolute, Spawn
+from turtlesim.srv import TeleportAbsolute, Spawn, Kill
 from turtlesim.msg import Pose as TPose #This line caused an unessasary amount of frustration
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
-from custom_msg.msg import ListString, Pos
+from custom_msg.msg import ListString
 
+#Look into assertions
+#
 class TurtleSimControl(Node):
     def __init__(self, namespace='turtle1'):
         super().__init__('turtle_sim_control')
         self.namespace = namespace
+    def initialize(self):
+
+        self.namespace = "turtle1"
+        self.filtered_service_list = []
+        self.servicess = []
         self.qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT,
                                  durability=DurabilityPolicy.VOLATILE,
                                  history=HistoryPolicy.KEEP_LAST,
                                  depth=1)
-        self.velocity_publisher = self.create_publisher(Twist, f'/{namespace}/cmd_vel', self.qos_profile)
-        self.teleport_client = self.create_client(TeleportAbsolute, f'/{namespace}/teleport_absolute')
+        
+        # self.velocity_publisher = self.create_publisher(Twist, f'/{namespace}/cmd_vel', self.qos_profile)
+        self.teleport_client = self.create_client(TeleportAbsolute, f'/{self.namespace}/teleport_absolute')
         self.spawn_client = self.create_client(Spawn, 'spawn')
-        self.teleportNode = self.create_subscription(TPose, '/'+ self.namespace + '/pose', self.update_pose, self.qos_profile)
-        self.serviceList = self.create_subscription(ListString, "/MANOS/ServiceDetector",self.getServices, self.qos_profile)
-        self.keywords = [] #Input Keywords for services here
-        self.spawned_turtles = []  
-
-    def move_turtle_to_goal(self, x, y):
-        twist_msg = Twist()
-        goal_x, goal_y = x, y
-        distance_tolerance = 0.05
-
-        while self.euclidean_distance(goal_x, goal_y) >= distance_tolerance:
-            linear_vel = self.linear_velocity(goal_x, goal_y)
-            angular_vel = self.angular_velocity(goal_x, goal_y)
-            twist_msg.linear.x = linear_vel
-            twist_msg.angular.z = angular_vel
-            self.velocity_publisher.publish(twist_msg)
-
-        # Stop the turtle after reaching the goal
-        twist_msg.linear.x = 0
-        twist_msg.angular.z = 0
-        self.velocity_publisher.publish(twist_msg)
-
+        self.kill_client = self.create_client(Kill, 'kill')
+        self.turtlePoseSubNode = self.create_subscription(TPose, '/'+ self.namespace + '/pose', self.update_pose, self.qos_profile)
+        self.serviceList = self.create_subscription(ListString, "/MANOS/ServiceDetector", self.getServices, self.qos_profile)
+        #Input Keywords for services here... 
+        #additionally HOST_LISTENER provides a list of services that returns a list of services from outside systems.
+        # IF you dont know all the services you can work with Just ros2 topic echo /MANOS/ServiceDetector.
+        # this also will filter out all the MANOS services. The keyword method is here so you DONT have 
+        #to implement all services when you are just looking to implement a few 
+        self.keywords = ['/clear', '/kill', '/reset', '/spawn',  '/turtle1/teleport_absolute',
+                          '/turtle1/teleport_relative'] #'/turtle1/set_pen' for example. 
+        self.spawned_turtles = [] 
+        
     def teleport_turtle(self, msg):
         teleport_request = TeleportAbsolute.Request()
-        teleport_request.x = 10- (msg.x / 640) * 10
+        teleport_request.x = 10 - (msg.x / 640) * 10
         teleport_request.y = 10 - (msg.y / 480) * 10  # Invert the y-coordinate
         teleport_request.theta = 0.0  # Ensure that the turtle does not change its orientation
         self.teleport_service.call_async(teleport_request)
@@ -53,64 +52,104 @@ class TurtleSimControl(Node):
         request.theta = self.calculate_theta(10-(msg.x / 640) * 10, 10 - (msg.y / 480) * 10)  # Invert the y-coordinate
         self.teleport_service.call_async(request)
 
-    def euclidean_distance(self, goal_x, goal_y):
-        current_x = 0.0  # Get the current turtle's x-position here
-        current_y = 0.0  # Get the current turtle's y-position here
-        return ((goal_x - current_x) ** 2 + (goal_y - current_y) ** 2) ** 0.5
+    def calculate_theta(self, x, y):
+        # Calculate the angle (theta) based on the finger position
+        current_x = self.pose.x
+        current_y = self.pose.y 
+        delta_x = x - current_x
+        delta_y = y - current_y
 
-    def linear_velocity(self, goal_x, goal_y, constant=1.5):
-        return constant * self.euclidean_distance(goal_x, goal_y)
+        # Calculate the angle using arctangent (in radians)
+        theta = math.atan2(delta_y, delta_x)
 
-    def angular_velocity(self, goal_x, goal_y, constant=6):
-        return constant * (self.steering_angle(goal_x, goal_y) - 0.0)
+        return theta
 
-    def steering_angle(self, goal_x, goal_y):
-        current_x = self.pose.x # Get the current turtle's x-position here
-        current_y = self.pose.y  # Get the current turtle's y-position here
-        return 0.0  # Calculate the steering angle
-    def kill_turtle(self, turtle_id):
-        if turtle_id in self.spawned_turtles:
-            self.node.destroy_subscription(old_subscription)
-            self.spawned_turtles.remove(turtle_id)
-            
-    def turtleSelection(self, msg):
+    def euclidean_distance(self, goal_pose):
+        """Euclidean distance between current pose and the goal."""
+        return math.sqrt(math.pow((goal_pose.position.x - self.pose.x), 2) +
+                    math.pow((goal_pose.position.y - self.pose.y), 2))
+
+    def linear_vel(self, goal_pose, constant=1.5):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return constant * self.euclidean_distance(goal_pose)
+
+    def steering_angle(self, goal_pose):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return math.atan2(goal_pose.position.y - self.pose.y, goal_pose.position.x - self.pose.x)
+
+    def angular_vel(self, goal_pose, constant=6):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
+
+    def move2goal(self,msg):
+        """Moves the turtle to the goal."""
+        goal_pose = TPose()
+        goal_pose.position.x = (msg.x/ 640) * 10
+        goal_pose.position.y = (msg.y/480) * 10
+        distance_tolerance =.05
+        vel_msg = Twist()
+
+        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+
+            # Proportional controller.
+            # https://en.wikipedia.org/wiki/Proportional_control
+
+            vel_msg.linear.x = self.linear_vel(goal_pose)
+            vel_msg.angular.z = self.angular_vel(goal_pose)
+            self.velocity_publisher.publish(vel_msg)
+
+        # Stopping our robot after the movement is over.
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+
+    def turleSel(self,name):
+        if self.namespace == name:
+            pass
+        else:
+            self.namespace = name
+            self.destroy_subscription(self.turtlePoseSubNode)
+            self.turtlePoseSubNode = self.create_subscription(TPose, '/'+ self.namespace + '/pose', self.update_pose, self.qos_profile)
+   
+    def turtleSelection(self, msg): 
         val = msg.data
         if val == 1:
-            self.namespace = 'turtle1'
+           self.turleSel('turtle1')
 
         elif val == 2:
-            self.namespace = 'turtle2'
             if 'turtle2' not in self.spawned_turtles:
                 self.spawned_turtles.append('turtle2')
                 self.spawn_turtle('turtle2', 5.0, 3.0, 0.0)
+            self.turleSel('turtle2')
+
         elif val == 3:
-            self.namespace = 'turtle3'
             if 'turtle3' not in self.spawned_turtles:
                self.spawned_turtles.append('turtle3')
                self.spawn_turtle('turtle3', 1.0, 4.0, 0.0)
+            self.turleSel('turtle3')
+
         elif val == 4 :
             self.namespace = 'turtle4'
             if 'turtle4' not in self.spawned_turtles:
                 self.spawned_turtles.append('turtle4')
                 self.spawn_turtle('turtle4', 8.0, 8.0, 0.0)
+            self.turleSel('turtle4')
+
         elif val == 5:
-            self.namespace = 'turtle5'
             if 'turtle5' not in self.spawned_turtles:
                self.spawned_turtles.append('turtle5')
                self.spawn_turtle('turtle5', 8.0, 5.0, 0.0)
+            self.turleSel('turtle5')
+
         return self.namespace
     
-    def kill_turtle_by_id(self, turtle_id):
-        if turtle_id == 1:
-            self.kill_turtle('turtle1')
-        elif turtle_id == 2:
-            self.kill_turtle('turtle2')
-        elif turtle_id == 3:
-            self.kill_turtle('turtle3')
-        elif turtle_id == 4:
-            self.kill_turtle('turtle4')
-        elif turtle_id == 5:
-            self.kill_turtle('turtle5')
+    
+    def kill_turtle(self, turtle_id):
+        self.spawned_turtles.remove(turtle_id)
+        self.temp = Kill.Request()
+        self.temp.name = turtle_id
+        self.kill_client.call_asyc(self.temp)   
+
 
     def spawn_turtle(self, name, x, y, theta):
         request = Spawn.Request()
@@ -125,19 +164,17 @@ class TurtleSimControl(Node):
         self.pose.x = round(data.x, 4)
         self.pose.y = round(data.y, 4)
 
-    def getServices(self,data):
-        self.services = data
-        self.filtered_service_list = [(service_name, service_type) for service_name, service_type in  self.services
-                                if any(keyword in service_name for keyword in self.keywords)]
+    def getServices(self, msg):
+        self.servicess = msg.data
+        self.filtered_service_list = [(service_name, service_type) for service_name, service_type in self.servicess
+                                      if any(keyword in service_name for keyword in self.keywords)]
 
-        return self.filtered_service_list
     
 def main(args=None):
     rclpy.init(args=args)
+    node = rclpy.create_node('turtleInterface')  # Create a ROS 2 Node instance
     turtle_control = TurtleSimControl()
-    # Use the methods of TurtleSimControl to control the turtle
     rclpy.spin(turtle_control)
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
